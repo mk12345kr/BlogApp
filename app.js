03b55cf15c345079560527c1f824d1930804fef3
mk12345kr <mk12345kr@gmail.com>
2020-03-15 17:21:02 +0530

    changle ejs syntax for importing file

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ express        = require("express"),
 app            = express();
 
 // APP CONFIG
-var DBURL=process.env.DBURL;
-mongoose.connect(DBURL||"mongodb://localhost/restful_blog_app");
+
+mongoose.connect(process.env.DBURL||"mongodb://localhost/restful_blog_app1");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
